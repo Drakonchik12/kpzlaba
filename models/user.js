d7909f3a1b171c109e0f9996873a29eea0001686
Vlad Ataman <bearlikecode@gmail.com>
2024-11-26 22:13:57 +0200

    Updated User model to sequelize

--- models/user.js
+++ models/user.js
@@ -1,63 +1,40 @@
-const pool = require('../db');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db'); // Import Sequelize instance
 
-// Добавить нового пользователя
-const addUser = (user, callback) => {
-  const sql = `INSERT INTO Users (username, email, password_hash, role, first_name, last_name, phone_number)
-               VALUES (?, ?, ?, ?, ?, ?, ?)`;
+const User = sequelize.define(
+  'User',
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    tableName: 'Users', // Match your database table name
+    timestamps: false, // Disable Sequelize's default createdAt/updatedAt fields
+  },
+);
 
-  pool.query(
-    sql,
-    [
-      user.username,
-      user.email,
-      user.password_hash,
-      user.role,
-      user.first_name,
-      user.last_name,
-      user.phone_number
-    ],
-    callback
-  );
-};
-
-// Получить всех пользователей
-const getAllUsers = (callback) => {
-  const sql = 'SELECT * FROM Users';
-  pool.query(sql, callback);
-};
-
-// Получить пользователя по ID
-const getUserById = (id, callback) => {
-  const sql = 'SELECT * FROM Users WHERE id = ?';
-  pool.query(sql, [id], callback);
-};
-
-// Обновить пользователя по ID
-const updateUser = (id, user, callback) => {
-  const sql = `UPDATE Users SET 
-               username = ?, email = ?, password_hash = ?, role = ?, first_name = ?, 
-               last_name = ?, phone_number = ? WHERE id = ?`;
-
-  pool.query(
-    sql,
-    [
-      user.username,
-      user.email,
-      user.password_hash,
-      user.role,
-      user.first_name,
-      user.last_name,
-      user.phone_number,
-      id
-    ],
-    callback
-  );
-};
-
-// Удалить пользователя по ID
-const deleteUser = (id, callback) => {
-  const sql = 'DELETE FROM Users WHERE id = ?';
-  pool.query(sql, [id], callback);
-};
-
-module.exports = { addUser, getAllUsers, getUserById, updateUser, deleteUser };
+module.exports = User;
